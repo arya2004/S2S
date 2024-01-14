ac3f37c8e56b3710a8de494b0cf3f6e004d29749
yashthipsay <yashthpsay@gmail.com>
2024-01-14 14:03:07 +0530

    dca

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:EU5C4bG2MpXsdUOR@cluster0.bpuumh0.mongodb.net/?retryWrites=true&w=majority";
+require('dotenv').config();
+const uri = `${process.env.MONGODB_URI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
