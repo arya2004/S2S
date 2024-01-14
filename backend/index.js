d05b30deb3505cf0cd104b90e8fb41dce5a08424
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2024-01-14 14:29:58 +0530

    backend:connect db and basic express setup

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,16 @@
+import express from 'express'
+import { connectDB } from './database/database.js'
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-const uri = `${process.env.MONGODB_URI}`;
+const app = express()
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+app.get("/",(req,res)=>{
+  res.json({message:"hello from s2s server"})
+})
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const PORT = process.env.PORT || 4000;
+
+connectDB().then(()=>{
+  app.listen(PORT,()=>{
+    console.log("server running on port:",PORT);
+  })
+})
