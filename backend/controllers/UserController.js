e299b5402fa7e8f7353ccf8b4ea1a6b8a87b644b
Yash Thipsay <yashthpsay@gmail.com>
2024-01-27 09:36:22 +0530

    crud applications

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,3 +13,35 @@ exports.createUser = async (req, res) => {
         res.status(500).json({message: error.message});
       }
 }
+
+exports.updateuser = async (req, res) => {
+    const { id } = req.params;
+  await User.updateOne({_id: id}, req.body);
+  const updatedUser = await DocumentFragment.findById(id);
+  return res.status(200).json(updatedUser);
+}
+
+exports.deleteUser = async(req, res) => {
+    const {id} = req.params;
+  const deletedUser = await User.findByIdAndDelete(id);
+  return res.status(200).json(deletedUser);
+}
+
+exports.getAllUsers = async(req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+}
+
+exports.getUser = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+      } catch(error) {
+        res.status(500).json({message: error.message});
+      }
+}
