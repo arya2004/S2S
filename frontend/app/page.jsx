137c0aba87e40b38ff8c98efba9303ea202b4469
Apoorva Pendse <apoorvavpendse@gmail.com>
2024-02-03 11:05:49 +0530

    ui : basic page

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,10 +1,34 @@
+"use client"
 import Image from "next/image";
-
+import Link from "next/link";
+import { motion } from "framer-motion"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className=" text-3xl font-bold">S2S</h1>
+      <motion.h1 animate={{y:-10}} className=" text-3xl font-bold">S2S</motion.h1>
+      <motion.div
+        initial={{ y: -100,opacity:0 }}
+        animate={{ y: 0,opacity:1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Image src="/home1.jpg" width={500} height={100} />
+      </motion.div>
       <h3>By the community for the community.</h3>
+      <motion.a
+        animate={{ opacity: 1 }}
+        className=" bg-[#2A9D8F]
+         opacity-50
+       px-2 rounded-sm
+      hover:text-[#E9C46A]
+      transition-all 
+      duration-200 
+      font-bold 
+      text-xl"
+        href={"/signup"}
+        
+      >
+        Get Started
+      </motion.a>
     </main>
   );
 }
---
+++ frontend/app/signup/page.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+const page = () => {
+  return (
+    <main className="flex h-[100vh] w-[100vw] bg-[#264653] justify-center items-center">
+        <div className=" border w-[24vw] h-[50vh] rounded-md"> 
+          <h1>Signup Page</h1>
+        </div>
+    </main>
+  )
+}
+
+export default page
