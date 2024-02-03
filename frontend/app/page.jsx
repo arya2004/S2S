d0fa31aec39ca9dc65da4c0815dabe02f20d02ce
Apoorva Pendse <apoorvavpendse@gmail.com>
2024-02-03 09:27:12 +0530

    setup frontend

---
+++ frontend/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className=" text-3xl font-bold">S2S</h1>
+      <h3>By the community for the community.</h3>
+    </main>
+  );
+}
