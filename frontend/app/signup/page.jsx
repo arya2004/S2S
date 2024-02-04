50b3e47591bdf4c19f23b8f516153909b54987f6
Yash Thipsay <yashthpsay@gmail.com>
2024-02-04 12:51:59 +0530

    particle auth

--- frontend/app/signup/page.jsx
+++ frontend/app/signup/page.jsx
@@ -1,9 +1,36 @@
+"use client"
+import '@particle-network/connect-react-ui/dist/index.css';
+import {ConnectButton} from '@particle-network/connect-react-ui'
 import React from "react"
 const page = () => {
   return (
     <main className="flex h-[100vh] w-[100vw] bg-[#264653] justify-center items-center">
         <div className=" border w-[24vw] h-[50vh] rounded-md"> 
-          <h1>Signup Page</h1>
+        <ConnectButton.Custom>
+            {({ account, chain, openAccountModal, openConnectModal, openChainModal, accountLoading }) => {
+                return (
+                    <div>
+                        <button onClick={openConnectModal} disabled={!!account}>
+                            Open Connect
+                        </button>
+                        <br />
+                        <br />
+                        <button onClick={openAccountModal} disabled={!account}>
+                            Open Account
+                        </button>
+                        <br />
+                        <br />
+                        <button onClick={openChainModal} disabled={!account}>
+                            Open Switch Network
+                        </button>
+                        <div>
+                            <h3>account</h3>
+                            <p>{account}</p>
+                        </div>
+                    </div>
+                );
+            }}
+        </ConnectButton.Custom>
         </div>
     </main>
   )
